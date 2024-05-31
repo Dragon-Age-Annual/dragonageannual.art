1cb7389cb2577fa63c67571c99e7f9c17a2c70eb
enigmalea <enigmalea@gmail.com>
2024-05-31 15:19:07 -0400

    updated landing page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
-import { Current, Intro, Preorder, Social, Thanks } from "@site/src/components/Homepage";
+import {
+  Current,
+  Intro,
+  Social,
+} from "@site/src/components/Homepage";
 
 import Form from "@site/src/components/NewsletterSignUp";
 import Layout from "@theme/Layout";
 import React from "react";
 import clsx from "clsx";
-import styles from "./index.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary")}>
-      <h1 className={styles.title}>Dragon Age Annual 2024</h1>
+      <img src="/img/daa_2025_banner.png" alt="Dragon Age 2025" />
     </header>
   );
 }
@@ -22,8 +25,6 @@ export default function Home(): JSX.Element {
       <HomepageHeader />
       <main>
         <Current />
-				{/* <Preorder /> */}
-				<Thanks />
         <Intro />
         <Social />
         <Form />
