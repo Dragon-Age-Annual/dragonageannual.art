d447428503c6e8e06fc4fa489aab4a14ffa7397d
enigmalea <admin@enigmalea.quest>
2026-04-19 00:35:45 -0400

    update to astro 6

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,47 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
+
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import metaTags from "astro-meta-tags";
+import partytown from "@astrojs/partytown";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import robotsTxt from "astro-robots-txt";
-
 import sitemap from "@astrojs/sitemap";
-
-import partytown from "@astrojs/partytown";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.dragonageannual.art",
   compressHTML: true,
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Lexend",
+      cssVariable: "--font-lexend",
+      fallbacks: ["sans-serif", "system-ui"],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Atkinson Hyperlegible",
+      cssVariable: "--font-atkinson-hyperlegible",
+      fallbacks: ["sans-serif", "system-ui"],
+    },
+    {
+      provider: fontProviders.local(),
+      name: "DAA Font",
+      cssVariable: "--font-daa",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/daa_font_regular-webfont.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
   integrations: [
     mdx(),
     icon(),
@@ -74,6 +101,7 @@ export default defineConfig({
   },
 
   vite: {
+		// @ts-expect-error: Vite's type definitions don't include the `plugins` property, but it is valid.
     plugins: [tailwindcss()],
   },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,10 @@
-import { defineCollection, z } from "astro:content";
-
-import { SocialLinks } from "@fujocoded/zod-transform-socials";
+import { SocialLinks } from "../zod-transform-socials/index.ts";
+import { defineCollection } from "astro:content"
+import { glob } from 'astro/loaders';
+import { z } from "astro/zod"
 
 const modCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: '**/*.md', base: "./src/content/mods/" }),
   schema: ({ image }) =>
     z.object({
       order: z.number(),
@@ -15,14 +16,14 @@ const modCollection = defineCollection({
 });
 
 const pastYearsCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: '**/*.md', base: "./src/content/pastYears/" }),
   schema: ({ image }) =>
     z.object({
       year: z.number().min(2022),
       title: z.string(),
       theme: z.string(),
       charityName: z.string(),
-      charityLink: z.string().url(),
+      charityLink: z.url(),
       cover: image(),
       coverAlt: z.string(),
       amountUS: z.number(),
